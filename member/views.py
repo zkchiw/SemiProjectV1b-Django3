e20d3d00e3e197284adf8286f1a96b4585bdd3fd
zkchiw <zkchiw@naver.com>
2022-06-21 15:17:11 +0900

    template file 수정 - home, member, board

--- member/views.py
+++ member/views.py
@@ -4,10 +4,8 @@ from django.shortcuts import render
 def join(request):
     return render (request, 'member/join.html')
 
-
 def login(request):
     return render (request, 'member/login.html')
-
 
 def myinfo(request):
     return render (request, 'member/myinfo.html')
