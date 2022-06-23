ff3920cc7c56470bf41e54de2e5e00b6edf817e4
zkchiw <zkchiw@naver.com>
2022-06-23 10:06:27 +0900

    django3 모델생성 - member

--- member/models.py
+++ member/models.py
@@ -1,3 +1,18 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    id=models.AutoField(primary_key=True)
+    userid=models.CharField(max_length=18, unique=True)
+    passwd=models.CharField(max_length=18)
+    name=models.CharField(max_length=5)
+    email=models.CharField(max_length=100)
+    regdate=models.DateTimeField(default=datetime.now)
+
+    class Meta :
+        db_table = 'member' # 테이블 생성시 이름 지정
+        ordering = ['-regdate'] # 정렬기준 설정
+
+    def __str__(self):
+        return self.userid  # 객체 출력시 출력형태 정의
+
