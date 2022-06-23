b4a7daf5c70cc7d1774bf1596ac6767a5005bd0d
zkchiw <zkchiw@naver.com>
2022-06-23 17:48:41 +0900

    django3 member 로그인/로그아웃 기능 구현
    세션처리, myinfo 연동

--- member/urls.py
+++ member/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     # http://127.0.0.1:8000/join/ 경로 요청시 member apps의 join함수가 처리함(views)
     path ('join/', views.join, name= 'join'),
     path ('login/', views.login, name= 'login'),
+    path ('logout/', views.logout, name= 'logout'),
     path ('myinfo/', views.myinfo, name= 'myinfo')
+
 ]
--- member/views.py
+++ member/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from member.models import Member
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
+from django.shortcuts import render, redirect
+
 # Create your views here.
 
 # 회원가입 처리 함수 join
@@ -17,7 +18,7 @@ def join(request):
         form = request.POST.dict()
         # print(form, form['userid']) # 전송된 데이터 확인(콘솔)
 
-        # 유효성 검사 1
+        # 유효성 검사
         error = '' # 검사 결과 저장용 변수 선언
         if  not (form['userid']and form['passwd']and form['passwdret']and form['name']and form['email']):
             error = '입력값이 누락되었습니다'
@@ -43,9 +44,56 @@ def join(request):
         context = {'form':form, 'error':error}
         return render (request, returnPage, context)
 
-
+# 로그인 처리 함수 login
 def login(request):
-    return render (request, 'member/login.html')
+    returnPage = 'member/login.html'
+    if request.method == 'GET':
+        return render(request, returnPage)
 
+    elif request.method == 'POST':
+        form = request.POST.dict()
+        error = ''
+        if  not (form['userid']and form['passwd']):
+            error = '아이디와 비밀번호를 입력해 주세요'
+        else:
+            # 입력한 아이디로 회원정보가 테이블에 존재하는지 여부 확인
+
+            try:
+                member = Member.objects.get(userid=form['userid'])
+            except Member.DoesNotExist:
+                member=None
+
+            if member and check_password(form['passwd'],member.passwd):
+                # 아이디와 비밀번호 인증을 정상적으로 마쳤다면 세션 변수에 인증정보를 저장해둠
+                request.session['userid'] = form['userid']
+
+                return redirect('/') # index 페이지로 이동
+            else :
+                error= '아이디나 비밀번호가 일치하지 않습니다'
+
+        context = {'error':error}
+        return render(request, returnPage, context)
+
+
+# 로그인한 회원 정보 출력 함수 myinfo
 def myinfo(request):
-    return render (request, 'member/myinfo.html')
+    # 로그인한 회원 아이디를 알아냄 - 세션변수
+    member = {}
+
+    if request.session.get('userid'):
+        userid = request.session.get('userid')
+
+    # 아이디를 이용해서 member 테이블에서 회원정보를 조회
+        member =Member.objects.get(userid=userid)
+
+    context = {'member':member}
+    return render (request, 'member/myinfo.html',context)
+
+# 로그아웃 처리 함수
+def logout(request):
+    # 만약 세션변수 userid가 존재하면 세션변수 삭제
+    if request.session.get('userid') :
+        del(request.session['userid'])
+
+    # 로그아웃시 index로 이동
+    return redirect('/')
